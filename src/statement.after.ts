c99419fccaf2eafeb346139160c98f8fc56ad532
hoon2hooni <sanghoon.engineer@gmail.com>
2021-08-18 19:47:51 +0900

    refac: seperate renderHtml and createStatement function

--- src/statement.after.ts
+++ src/statement.after.ts
@@ -1,20 +1,21 @@
-
 import { Invoice, Plays, Performance, Play } from './types';
 import createStatementData from './createStatementData';
+
 export default function statement(invoice: Invoice, plays: Plays): string {
   const statementData: Invoice = createStatementData(invoice, plays);
-  
-  return renderPlainText(statementData, plays);
 
-  function renderPlainText(data: Invoice, plays: Plays) {
-    let result = `청구내역 (고객명: ${data.customer})\n`;
+  return renderHtml(statementData);
+
+  function renderHtml(data: Invoice) {
+    let result = `<h1>청구내역 (고객명: ${data.customer})</h1>\n`;
+    result += `<table>\n`;
+    result += `<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>`;
     for (let perf of data.performances) {
-      result += `${perf.play.name} : ${usd(perf.amount)} (${
-        perf.audience
-      }석)\n`;
+      result += ` <tr><td>${perf.play.name}</tr></td>(${perf.audience}석)`;
+      result += `<tr><td>${usd(perf.amount)}</tr></td>\n`;
     }
-    result += `총액: ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
     return result;
   }
 
