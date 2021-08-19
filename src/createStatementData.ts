fc8323d87f2157f8605156d661276c865e8c21fc
hoonihan <hooni.han@bunjang.co.kr>
2021-08-19 12:04:06 +0900

    refac: use Polymorphism of class instead of conditional statement

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -44,9 +44,36 @@ class PerformanceCalculator {
     return result;
   }
 }
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
 
-function createPerformanceCalculator(aPerformance : Performance, aPlay : Play) {
-  return new PerformanceCalculator(aPerformance, aPlay)
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result
+  }
+}
+
+function createPerformanceCalculator(aPerformance: Performance, aPlay: Play) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르 ${aPlay.type}`);
+  }
 }
 export default function createStatementData(invoice: Invoice, plays: Plays) {
   const result: Invoice = { ...invoice };
